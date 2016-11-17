0fd5194e7a6ce6d12bd72259035e551d4ffe4314
Jister <chenjiesjtu14@gmail.com>
2016-11-17 16:21:16 +0800

    sonar_filter model update

--- src/sonar_filter.cpp
+++ src/sonar_filter.cpp
@@ -14,14 +14,14 @@ private:
 	ros::Subscriber sonar_sub;
 	ros::Publisher sonar_filtered_pub;
 
-	float _A;
-	float _H;
-	float _R;
-	float _Q;
-	float P;
-	float K;
-	float X;
-	float X_predict;
+	Matrix2f _A;
+	Matrix<float,1,2> _H;
+	MatrixXf _Q;
+	MatrixXf _R;
+	MatrixXf P;
+	MatrixXf K;
+	Vector2f X;
+	Vector2f X_predict;
 	float Z;
 	
 	bool initialized;
@@ -34,10 +34,13 @@ SonarFilter::SonarFilter()
 	sonar_sub = n.subscribe("/sonar", 1, &SonarFilter::sonarCallback, this);
 	sonar_filtered_pub = n.advertise<std_msgs::Float32>("/sonar_filtered", 1);
 	initialized = false;
-	_A = 1;
-	_H = 1;
-	_Q = 1e-8;
-	_R = 1e-2;
+	_A << 1, 1,
+		  0, 1;
+	_H << 1, 0;
+	_Q = MatrixXf::Identity(2,2) * 1e-4;
+	_R = MatrixXf::Identity(1,1) * 1e-2;
+	P = MatrixXf::Identity(2,2);
+	K = MatrixXf::Zero(2,2);
 
 }
 
@@ -49,22 +52,21 @@ void SonarFilter::sonarCallback(const std_msgs::Float32 msg)
 	{
 		//predict
 		X_predict = _A * X;
-		P = _A * P * _A + _Q;
+		P = _A * P * _A.transpose() + _Q;
 		//correct
-		K = P * _H * 1 / (_H * P * 1 / _H + _R);
+		K = P * _H.transpose() * (_H * P * _H.transpose() + _R).inverse();
 		X = X_predict + K * (Z - _H * X_predict);
 		P = P - K * _H * P;
 
 	}else
 	{
-		X = msg.data;
-		K = 0;
-		P = 0;
+		X(0) = msg.data;
+		X(1) = 0;
 		initialized = true;
 	}
 
 	std_msgs::Float32 sonar_filtered;
-	sonar_filtered.data = X;
+	sonar_filtered.data = X(0);
 	sonar_filtered_pub.publish(sonar_filtered);
 
 }
